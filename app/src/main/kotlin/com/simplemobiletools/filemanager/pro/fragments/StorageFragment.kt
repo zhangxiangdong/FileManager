d76f283375f449001a52bfa808032a251c841328
zhangxiangdong <zhanglts@163.com>
2022-10-27 14:34:40 +0800

    Revert "存储页面样式优化"
    This reverts commit bbf022bf7ae945031804e0f8ea5fdea37b9e884a.

--- app/src/main/kotlin/com/simplemobiletools/filemanager/pro/fragments/StorageFragment.kt
+++ app/src/main/kotlin/com/simplemobiletools/filemanager/pro/fragments/StorageFragment.kt
@@ -21,10 +21,7 @@ import kotlinx.android.synthetic.main.storage_fragment.view.*
 import java.util.*
 
 class StorageFragment(context: Context, attributeSet: AttributeSet) : MyViewPagerFragment(context, attributeSet) {
-
-    companion object {
-        private const val SIZE_DIVIDER = 100000
-    }
+    private val SIZE_DIVIDER = 100000
 
     override fun setupFragment(activity: SimpleActivity) {
         total_space.text = String.format(context.getString(R.string.total_storage), "…")
@@ -56,6 +53,30 @@ class StorageFragment(context: Context, attributeSet: AttributeSet) : MyViewPage
         val properPrimaryColor = context.getProperPrimaryColor()
         main_storage_usage_progressbar.setIndicatorColor(properPrimaryColor)
         main_storage_usage_progressbar.trackColor = properPrimaryColor.adjustAlpha(0.3f)
+
+        val redColor = context.resources.getColor(R.color.md_red_700)
+        images_progressbar.setIndicatorColor(redColor)
+        images_progressbar.trackColor = redColor.adjustAlpha(0.3f)
+
+        val greenColor = context.resources.getColor(R.color.md_green_700)
+        videos_progressbar.setIndicatorColor(greenColor)
+        videos_progressbar.trackColor = greenColor.adjustAlpha(0.3f)
+
+        val lightBlueColor = context.resources.getColor(R.color.md_light_blue_700)
+        audio_progressbar.setIndicatorColor(lightBlueColor)
+        audio_progressbar.trackColor = lightBlueColor.adjustAlpha(0.3f)
+
+        val yellowColor = context.resources.getColor(R.color.md_yellow_700)
+        documents_progressbar.setIndicatorColor(yellowColor)
+        documents_progressbar.trackColor = yellowColor.adjustAlpha(0.3f)
+
+        val tealColor = context.resources.getColor(R.color.md_teal_700)
+        archives_progressbar.setIndicatorColor(tealColor)
+        archives_progressbar.trackColor = tealColor.adjustAlpha(0.3f)
+
+        val pinkColor = context.resources.getColor(R.color.md_pink_700)
+        others_progressbar.setIndicatorColor(pinkColor)
+        others_progressbar.trackColor = pinkColor.adjustAlpha(0.3f)
     }
 
     private fun launchMimetypeActivity(mimetype: String) {
@@ -71,10 +92,7 @@ class StorageFragment(context: Context, attributeSet: AttributeSet) : MyViewPage
         }
 
         ensureBackgroundThread {
-            try {
-                getMainStorageStats(context)
-            } catch (_: Exception) {
-            }
+            getMainStorageStats(context)
 
             val filesSize = getSizesByMimeType()
             val imagesSize = filesSize[IMAGES]!!
@@ -86,11 +104,22 @@ class StorageFragment(context: Context, attributeSet: AttributeSet) : MyViewPage
 
             post {
                 images_size.text = imagesSize.formatSize()
+                images_progressbar.progress = (imagesSize / SIZE_DIVIDER).toInt()
+
                 videos_size.text = videosSize.formatSize()
+                videos_progressbar.progress = (videosSize / SIZE_DIVIDER).toInt()
+
                 audio_size.text = audioSize.formatSize()
+                audio_progressbar.progress = (audioSize / SIZE_DIVIDER).toInt()
+
                 documents_size.text = documentsSize.formatSize()
+                documents_progressbar.progress = (documentsSize / SIZE_DIVIDER).toInt()
+
                 archives_size.text = archivesSize.formatSize()
+                archives_progressbar.progress = (archivesSize / SIZE_DIVIDER).toInt()
+
                 others_size.text = othersSize.formatSize()
+                others_progressbar.progress = (othersSize / SIZE_DIVIDER).toInt()
             }
         }
     }
@@ -138,10 +167,10 @@ class StorageFragment(context: Context, attributeSet: AttributeSet) : MyViewPage
                             }
                         }
                     }
-                } catch (_: Exception) {
+                } catch (e: Exception) {
                 }
             }
-        } catch (_: Exception) {
+        } catch (e: Exception) {
         }
 
         val mimeTypeSizes = HashMap<String, Long>().apply {
@@ -172,7 +201,8 @@ class StorageFragment(context: Context, attributeSet: AttributeSet) : MyViewPage
 
                 post {
                     arrayOf(
-                        main_storage_usage_progressbar
+                        main_storage_usage_progressbar, images_progressbar, videos_progressbar, audio_progressbar, documents_progressbar,
+                        archives_progressbar, others_progressbar
                     ).forEach {
                         it.max = (totalSpace / SIZE_DIVIDER).toInt()
                     }
@@ -184,6 +214,10 @@ class StorageFragment(context: Context, attributeSet: AttributeSet) : MyViewPage
                     total_space.text = String.format(context.getString(R.string.total_storage), totalSpace.formatSizeThousand())
                     free_space_label.beVisible()
                 }
+            } else {
+                // sd card
+                val totalSpace = file.totalSpace
+                val freeSpace = file.freeSpace
             }
         }
     }
